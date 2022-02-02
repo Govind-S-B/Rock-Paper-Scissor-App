1bb1fa67c3d2db943321539dda3f88d14ccd834f
Govind-S-B <b.s.dnivog@gmail.com>
2022-02-02 22:16:33 +0530

    made ui

---
+++ app/src/main/java/com/example/rock_paper_scissor_app/Final.kt
@@ -0,0 +1,11 @@
+package com.example.rock_paper_scissor_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Final : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_final)
+    }
+}
