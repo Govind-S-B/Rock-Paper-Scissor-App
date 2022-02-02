53b76105c4b4f94798f7bf3f45cbc03d82c7c7f8
Govind-S-B <b.s.dnivog@gmail.com>
2022-02-02 16:58:14 +0530

    added primary screen functionality , removed action bar

---
+++ app/src/main/java/com/example/rock_paper_scissor_app/Game.kt
@@ -0,0 +1,11 @@
+package com.example.rock_paper_scissor_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Game : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
--- app/src/main/java/com/example/rock_paper_scissor_app/MainActivity.kt
+++ app/src/main/java/com/example/rock_paper_scissor_app/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.rock_paper_scissor_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private var rounds : Int = 3 // initiating the variable with a dummy value
+    private lateinit var input3 : Button
+    private lateinit var input5 : Button
+    private lateinit var input7 : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        input3 = findViewById(R.id.button3)
+        input5 = findViewById(R.id.button5)
+        input7 = findViewById(R.id.button7)
+
+        buttonListeners()
+
+    }
+
+    fun buttonListeners(){
+
+        var intent = Intent(this,Game::class.java)
+
+        input3.setOnClickListener {
+            rounds = 3
+            intent.putExtra("score",rounds)
+            startActivity(intent)
+            finish()
+        }
+
+        input5.setOnClickListener {
+            rounds = 5
+            intent.putExtra("score",rounds)
+            startActivity(intent)
+            finish()
+        }
+
+        input7.setOnClickListener {
+            rounds = 7
+            intent.putExtra("score",rounds)
+            startActivity(intent)
+            finish()
+        }
     }
 }
